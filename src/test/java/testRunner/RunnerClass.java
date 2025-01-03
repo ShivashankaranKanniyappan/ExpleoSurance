96139eb5278e750bfa4f9a8320fa02857ff301e7
shiva <shivashankaran.kanniyappan@expleogroup.com>
2025-01-03 20:00:33 +0530

    new updates

--- src/test/java/testRunner/RunnerClass.java
+++ src/test/java/testRunner/RunnerClass.java
@@ -8,7 +8,8 @@ import io.cucumber.testng.CucumberOptions.SnippetType;
 				features = "src\\test\\resources\\loginScenario.feature", 
 				glue = {"stepDefinition", "hooks"}, 
 				snippets = SnippetType.CAMELCASE, 
-				plugin = {"pretty", "json:target/cucumber.json"},
+				plugin = {"pretty", "json:target/cucumber.json", 
+									"html:target/htmlreport.html"},
 				dryRun = false
 				)
 public class RunnerClass extends AbstractTestNGCucumberTests 
