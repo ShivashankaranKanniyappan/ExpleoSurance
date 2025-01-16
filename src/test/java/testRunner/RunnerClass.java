a9585f83a019fac73886a0d9c8a3bf74c0adc00d
ShivaShankaran <shivashankaran.kanniyappan@expleogroup.com>
2025-01-16 16:06:03 +0530

    chanegs in runner file

--- src/test/java/testRunner/RunnerClass.java
+++ src/test/java/testRunner/RunnerClass.java
@@ -5,12 +5,12 @@ import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.CucumberOptions.SnippetType;
 
 @CucumberOptions(
-		features = "src/test/resources/feature13_paymentPage.feature",
+		features = "src/test/resources",
 		glue = {"stepDefinition", "hooks"}, 
 		snippets = SnippetType.CAMELCASE, 
 		plugin = {"pretty", "json:target/cucumber.json", 
 							"html:target/htmlreport.html"},
-	//	tags = "@All_Scenarios",
+		tags = "@All_Scenarios",
 		dryRun = false,
 		monochrome = false
 		)
